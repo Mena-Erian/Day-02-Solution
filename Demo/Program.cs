80ec1ad1516b543491f02cfad83de1b198d4efbd
Mena Erian Farouk <135561286+Mena-Erian@users.noreply.github.com>
2025-06-21 15:34:18 +0300

    Part 04 Variable Declaration and Comments

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,9 +2,40 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        //Entry Point
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+           
+            int Number = 10;
+            short Num = 4;
+            // Not Recommend
+            //Console.WriteLine("Number = " + Number);
+            //Console.WriteLine(); //cw + tap
+            //you can meke your snipt
+            Console.WriteLine("Numberb = {0} ,{2} {1}",Number,"Mon","Name");
+            //   string interpolation
+            Console.WriteLine($"Number = {Number}");
         }
     }
 }
+//single line Comment
+/**
+ * Mulitline
+ * of
+ * Comments
+ **/
+/// re /s
+/// 
+
+//You Should to know somethings when you Declaring Variable
+/**
+ * Data Type    |   Name    |   Value   |   Address
+ * ing          |   x       |   5       |   1000
+ * to know the  |Any key 
+ * size it will |word in C# 
+ * be taked in  |Don't have 
+ * memory       |Kabetal letter
+ *              |Pascal Case
+ *              |Camel Case
+ *              |
+ */

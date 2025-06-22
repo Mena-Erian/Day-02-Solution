a62e3c402ce7f38e4391ec4065997b38f6cbcad5
Mena Erian Farouk <135561286+Mena-Erian@users.noreply.github.com>
2025-06-22 19:12:06 +0300

    From Part 08 to Part 11 ValueType Vs ReferenceType

---
+++ Demo/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Point
+    {
+        public int x { get; set; } //= 0; // 4 byte
+        public int y { get; set; } //= default(int);// 4 byte
+    }
+    class Point3D : Point
+    {
+        public int z { get; set; }  // 4 byte
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -24,14 +24,14 @@ namespace Demo
              * to know the  |Any key 
              * size it will |word in C# 
              * be taked in  |Don't have 
-             * memory       |Kabetal letter
+             * memory       |Kabetal letter and we use those naming convinsion =>
              *              |Pascal Case
              *              |Camel Case
              *              |
              */
             //int Number = 10;//int32
             //short Num = 4;//int16
-            // Not Recommend
+            // Not Recommend use this => int32 
             //Console.WriteLine("Number = " + Number);
             //Console.WriteLine(); //cw + tap
             //you can meke your snipt
@@ -47,7 +47,7 @@ namespace Demo
              * CTS => A full set of types the CLR(Common Language Runtime) understands—foundation for all .NET languages
              * 1- Broader system defining how types(value types, reference types, enums
              *,classes, delegates) are represented and behave in memory and metadata
-             * 2- Ensures type safety, enforcing rules like all types deriving from System.Object, consistent memory layout, inheritance behaviors, etc. .
+             * 2- Ensures type safety(ex..int+string), enforcing rules like all types deriving from System.Object, consistent memory layout, inheritance behaviors, etc. .
              * 3- Language interoperability: C#’s int, VB’s Integer, or F#’s int all map to the same CTS type (System.Int32) at runtime
              **/
             /**
@@ -62,17 +62,63 @@ namespace Demo
             // Have 2 Categories:
             /// Based on How it will be saved in Memory
             // 1- Value Type (Primitive) => saved in Stack like {Struct | Enum}
-            // 2- Reference Type (NonPrimitive) => Value in Heep & the value of
+            // 2- Reference Type (NonPrimitive) => Value saved in Heep & the value of
             //    Address or the references in Stack ( Always take 4 byte ) like {Class | interface}
             /**
              * CLR that have the components that Provide the Momery nead of app and 
              * this divide into stack and heep
-             * stack take 1MP & Heap Shrink and deShrink depanded of Memorey Nead
+             * stack take 1MP & Heap Shrink and deShrink depanded of Memorey Need
              */
-            //GC (Garbage Collection) That Component from CLR it control Memory at runtime 
+            //GC (Garbage Collection) That Component from CLR it control Memory at runtime
+
+            #endregion
+            #region Value Type
+            //int x;
+            /// Declare for variable of type "integer"
+            /// CLR will Allocate of Unitillized bytes in Stack.
+            /// int: C# Keyword
+            //x = 5;
+            //Int32 y = 10;
+            /// Declare for variable of type "integer"
+            /// CLR will Allocate of Unitillized bytes in Stack.
+            /// int32: BCL => Base Class Library
+            //y = x;
+            //x++;
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            #endregion
+            #region Reference Type
+            //Point p1;
+            /// Declare for refernce of type "Point", Refering to the Default Value of the Reference that "Null"
+            /// This reference P1 Can refer to an Object of type point of another type inheriting from type "Point"
+            /// CLR will Allocate 4 byte in Stack for the reference "P1".
+            /// CLR will Allocate 0 byte in HEAP.
+            //also can do this p1=new()
+            //p1 = new Point();
+            //new
+            /// 1.Allocate the Required Number of Bytes For the Object at HEAP (Object Size + CLR Overhead Variables)
+            /// 2.Initilize the Allocated Bytes with The defult value of its datatype
+            /// 3.Call User-Defined Constructor [if exists]
+            /// 4.Return the Address of the Allocated obeject to the reference 'p1' in Stack
+
+            //Console.WriteLine(p1.x);    //0
+            //Console.WriteLine(p1.y);    //0
+            //Point p2 = new Point() { x = 5, y = 10 };
+            //Console.WriteLine("After Initilize P2");
+            //Console.WriteLine(p2.x);
+            //Console.WriteLine(p2.y);
+            //p2 = p1;  // so Now the P1 Have 2 References //and the p2 will be UnReachable Object
+            //Console.WriteLine("After Assisgn P2 to P1");
+            //Console.WriteLine(p2.x);
+            //Console.WriteLine(p2.y);
+
+
+            //Console.WriteLine(default(char));
             #endregion
             #endregion
 
         }//done part 07 Application Memory
+
+
     }
 }
